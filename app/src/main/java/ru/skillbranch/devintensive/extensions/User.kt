4f490282a061b24344777b5ecf6a26daf0ee47ec
tswet <mtswetkov@gmail.com>
2019-07-04 11:01:48 +0300

    commit2

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -3,6 +3,7 @@ package ru.skillbranch.devintensive.extensions
 import ru.skillbranch.devintensive.models.UserView
 import ru.skillbranch.devintensive.models.User
 import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
 
 
 fun User.toUserView(): UserView {
@@ -24,6 +25,7 @@ fun User.toUserView(): UserView {
         status = status
     )
 }
+
 
 
 
